faf8161ea2ae4f04aa8804fd99b3620d6070ad7b
Njb <sdr.expert@gmail.com>
2024-11-26 20:24:56 +0200

    app well done

--- src/App.vue
+++ src/App.vue
@@ -9,63 +9,20 @@
                   <div class="city-inner">
                     <input v-model="city" @keyup.enter="getWeather" type="text" class="search">
                   </div>
+                  <!--! WeatherSummary  -->
                   <WeatherSummary :weather="weather"/>
                 </div>
               </section>
               <section class="section section-right">
-                <!--! highlight  -->
-                <HighLights />
+                <!--! Highlight  -->
+                <HighLights :weather="weather" />
               </section> 
             </div>
-            <div class="sections">
-              <section class="section-bottom">
-                <div
-                  class="block-bottom"
-                >
-                  <div class="block-bottom-inner">
-                    <div class="block-bottom-pic pic-coords"></div>
-                    <div class="block-bottom-texts">
-                      <div class="block-bottom-text-block">
-                        <div class="block-bottom-text-block-title">
-                          Longitude: 2.3488
-                        </div>
-                        <div class="block-bottom-text-block-desc">
-                          Longitude measures distance east or west of the prime meridian.
-                        </div>
-                      </div>
-                      <div class="block-bottom-text-block">
-                        <div class="block-bottom-text-block-title">
-                          Latitude: 48.8534
-                        </div>
-                        <div class="block-bottom-text-block-desc">
-                          Latitude lines start at the equator (0 degrees latitude) and run east and west, parallel to the equator. 
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-              <section class="section-bottom">
-                <div
-                  class="block-bottom"
-                >
-                  <div class="block-bottom-inner">
-                    <div class="block-bottom-pic pic-humidity"></div>
-                    <div class="block-bottom-texts">
-                      <div class="block-bottom-text-block">
-                        <div class="block-bottom-text-block-title">
-                          Humidity: 60 %
-                        </div>
-                        <div class="block-bottom-text-block-desc">
-                          Humidity is the concentration of water vapor present in the air. Water vapor, the gaseous state of water, is generally invisible to the human eye.
-                          <br /><br />
-                          The same amount of water vapor results in higher relative humidity in cool air than warm air.
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
+            <div class="sections" v-if="weather?.weather">
+              <!--! Coords  -->
+              <WeatherCoords :coord="weather?.coord" />              
+              <!--! Humidity  -->
+              <WeatherHumidity :humidity="weather?.main?.humidity"/>
             </div>
           </div>
         </div>
@@ -77,6 +34,8 @@
 import { ref, onMounted } from 'vue'; // Импорт API из Vue
 import WeatherSummary from './components/WeatherSummary.vue';
 import HighLights from './components/HighLights.vue';
+import WeatherCoords from './components/WeatherCoords.vue';
+import WeatherHumidity from './components/WeatherHumidity.vue';
 import {apiKey, baseUrl} from './constants'
 
 const city = ref('Kyiv')
@@ -94,7 +53,7 @@ function getWeather() {
       })
       .then(data => {
         weather.value = data;
-        console.log('Weather data:', data); // Вывод данных в консоль
+        console.table('Weather data:', data); // Вывод данных в консоль
       })
       .catch(error => {
         // Обработка ошибок запроса
---
+++ src/components/WeatherCoords.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="section-bottom">
+    <div class="block-bottom">
+      <div class="block-bottom-inner">
+        <div class="block-bottom-pic pic-coords"></div>
+        <div class="block-bottom-texts">
+          <div class="block-bottom-text-block">
+            <div class="block-bottom-text-block-title">
+              Longitude: {{ coord?.lon || 0 }}
+            </div>
+            <div class="block-bottom-text-block-desc">
+              Longitude measures distance east or west of the prime meridian.
+            </div>
+          </div>
+          <div class="block-bottom-text-block">
+            <div class="block-bottom-text-block-title">
+              Latitude: {{ coord?.lat || 0 }}
+            </div>
+            <div class="block-bottom-text-block-desc">
+              Latitude lines start at the equator (0 degrees latitude) and run east and west, parallel to the equator.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+defineProps({
+  coord: {
+    type: Object,
+    required: true,
+    default: () => ({ lon: 0, lat: 0 }),
+  },
+});
+</script>
+
+<style scoped>
+/* Добавьте стили, если нужно */
+</style>
