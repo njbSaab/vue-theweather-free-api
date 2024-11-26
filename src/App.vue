8ecc5531880c15cd3f0d4e665792a6fd4456a0b6
Njb <sdr.expert@gmail.com>
2024-11-26 06:29:26 +0200

    added connect with api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,210 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="page">
+      <main class="main">
+        <div class="container">
+          <div class="laptop">
+            <div class="sections">
+              <section class="section section-left">
+                <div class="info">
+                  <div class="city-inner">
+                    <input v-model="city" @keyup.enter="getWeather" type="text" class="search">
+                  </div>
+                  <WeatherSummary :weather="weather"/>
+                </div>
+              </section>
+              <section class="section section-right">
+                <!--! highlight  -->
+                <HighLights />
+              </section> 
+            </div>
+            <div class="sections">
+              <section class="section-bottom">
+                <div
+                  class="block-bottom"
+                >
+                  <div class="block-bottom-inner">
+                    <div class="block-bottom-pic pic-coords"></div>
+                    <div class="block-bottom-texts">
+                      <div class="block-bottom-text-block">
+                        <div class="block-bottom-text-block-title">
+                          Longitude: 2.3488
+                        </div>
+                        <div class="block-bottom-text-block-desc">
+                          Longitude measures distance east or west of the prime meridian.
+                        </div>
+                      </div>
+                      <div class="block-bottom-text-block">
+                        <div class="block-bottom-text-block-title">
+                          Latitude: 48.8534
+                        </div>
+                        <div class="block-bottom-text-block-desc">
+                          Latitude lines start at the equator (0 degrees latitude) and run east and west, parallel to the equator. 
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section class="section-bottom">
+                <div
+                  class="block-bottom"
+                >
+                  <div class="block-bottom-inner">
+                    <div class="block-bottom-pic pic-humidity"></div>
+                    <div class="block-bottom-texts">
+                      <div class="block-bottom-text-block">
+                        <div class="block-bottom-text-block-title">
+                          Humidity: 60 %
+                        </div>
+                        <div class="block-bottom-text-block-desc">
+                          Humidity is the concentration of water vapor present in the air. Water vapor, the gaseous state of water, is generally invisible to the human eye.
+                          <br /><br />
+                          The same amount of water vapor results in higher relative humidity in cool air than warm air.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, onMounted } from 'vue'; // Импорт API из Vue
+import WeatherSummary from './components/WeatherSummary.vue';
+import HighLights from './components/HighLights.vue';
+import {apiKey, baseUrl} from './constants'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const city = ref('Kyiv')
+const weather = ref(null)
+
+function getWeather() {
+  try {
+    fetch(`${baseUrl}?q=${city.value}&appid=${apiKey}`)
+      .then(response => {
+        // Проверяем, был ли запрос успешным
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        weather.value = data;
+        console.log('Weather data:', data); // Вывод данных в консоль
+      })
+      .catch(error => {
+        // Обработка ошибок запроса
+        console.error('Error fetching weather data:', error);
+      });
+  } catch (error) {
+    // Обработка ошибок вне fetch
+    console.error('Unexpected error:', error);
   }
 }
+
+onMounted(getWeather);
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+@use './assets/styles/main';
+
+.page {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 20px 0;
+  background: linear-gradient(120deg, #39139B 0%, #0D678E 100%);
 }
+
+.laptop {
+  width: 100%;
+  padding: 20px;
+  background-color: #0e100f;
+  border-radius: 25px;
+}
+
+.sections {
+  display: flex;
+  width: 100%;
+
+  @media (max-width: 767px) {
+    flex-direction: column;
+  }
+}
+
+.section-left {
+  width: 30%;
+  padding-right: 10px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    padding-right: 0;
+  }
+}
+
+.section-right {
+  width: 70%;
+  padding-left: 10px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    margin-top: 16px;
+    padding-left: 0;
+  }
+}
+
+.city-inner {
+  position: relative;
+  display: inline-block;
+  width: 100%;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 10px;
+    width: 25px;
+    height: 25px;
+    background: url('./assets/img/search.svg') no-repeat 50% 50%;
+    background-size: contain;
+    transform: translateY(50%);
+    cursor: pointer;
+  }
+}
+
+.info {
+  height: 100%;
+  padding: 16px;
+  background: url('./assets/img/gradient-1.jpg') no-repeat 50% 50%;
+  background-size: cover;
+  border-radius: 25px;
+}
+
+.search {
+  width: 100%;
+  padding: 16px;
+  font-family: 'Inter', Arial, sans-serif;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 16px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+
+.section-bottom {
+  width: 50%;
+  margin-top: 16px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+}
+
 </style>
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const apiKey = "ce12e1f762285c3ba9db61febd64fb24";
+export const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
